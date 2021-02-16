4f5c8ed46a5b9e6e52bdd2e13b0e15d417147c76
Nishka <Nishka.Ayyar.23@cmc.edu>
2021-02-15 18:41:24 -0800

    HW trial 2

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -40,7 +40,7 @@ def _extract_tags(html):
     By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
 
     This function returns a list of all the html tags contained in the input string,
-    stripping out all text not contained within angle brackets.
+    stripping out all text not contained within the angle brackets.
 
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
